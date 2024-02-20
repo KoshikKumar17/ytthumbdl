156b5d0c5f81ff54f4747438004079d28ddf2dab
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-02-20 14:14:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,10 +40,16 @@ download_btn.addEventListener("click", function() {
 
   function extractVideoId(url) {
     var videoId = null;
-    var pattern = /(?:https?:\/\/(?:www\.)?)?(?:youtu(?:be\.com\/(?:watch\?v=|embed\/|v\/)|\.be\/)([\w\-]+)|youtube\.com\/shorts\/[a-zA-Z0-9_-]+(\?[^#\s]*)?(?:[\?&]t=([\dhms]+))?)/i;
+    var pattern = /(?:https?:\/\/(?:www\.)?)?(?:youtu(?:be\.com\/(?:watch\?v=|embed\/|v\/)|\.be\/)([\w\-]+)|youtube\.com\/shorts\/([a-zA-Z0-9_-]+))/i;
     var match = url.match(pattern);
-    if (match && match[1].length === 11) {
-      videoId = match[1];
+    if (match) {
+      if (match[1]) {
+        if (match[1].length === 11) {
+          videoId = match[1]; // Video ID
+        }
+      } else if (match[2]) {
+        videoId = match[2]; // Shorts ID
+      }
     }
     return videoId;
   }
